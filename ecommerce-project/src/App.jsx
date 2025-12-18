bf8a244168a7d877ff684716e6a5b5d2df503a8d
kuroosh  sarvandi <kurooshsarvandi75@gmail.com>
2025-12-18 17:52:53 +0330

    Split header into the component and use Link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,8 @@
 import {Routes , Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { ChekoutPage } from './pages/ChekoutPage'
-
+import { OrderPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <Routes>
        <Route index element={<HomePage />}/>
        <Route path="chekout" element={<ChekoutPage/>}/>
+       <Route path="Order" element={<OrdersPage/>}/>
+       <Route path="TrackingPage" element={<TrackingPage/>}/>
     </Routes>
    
   )
