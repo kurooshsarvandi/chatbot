882997dd331617da106f99c8c0c4013407aa1228
kuroosh  sarvandi <kurooshsarvandi75@gmail.com>
2025-12-23 17:18:04 +0330

    Add xyserver proxy config for /api and /images

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,36 +1,32 @@
 import { Link } from 'react-router-dom';
 import './header.css';
-export function Header({ cart }) {
+
+export function Header({ cart = [] }) { // ۱. مقدار پیش‌فرض را آرایه خالی بگذارید
   let totalQuantity = 0;
 
-  cart.forEach((cartItem) => {
-     totalQuantity +=cartItem.quantity
+  // ۲. استفاده از optional chaining برای اطمینان از وجود cart
+  cart?.forEach((cartItem) => {
+     totalQuantity += cartItem.quantity;
   });
+
   return (
     <>
       <div className="header">
         <div className="left-section">
           <Link to="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
+            {/* ۳. یک اسلش اول آدرس عکس‌ها بگذارید تا در تمام صفحات به درستی لود شوند */}
+            <img className="logo" src="/images/logo-white.png" />
+            <img className="mobile-logo" src="/images/mobile-logo-white.png" />
           </Link>
         </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
+        {/* بقیه کد شما ... */}
         <div className="right-section">
           <Link className="orders-link header-link" to="/orders">
             <span className="orders-text">Orders</span>
           </Link>
 
           <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
+            <img className="cart-icon" src="/images/icons/cart-icon.png" />
             <div className="cart-quantity">{totalQuantity}</div>
             <div className="cart-text">Cart</div>
           </Link>
